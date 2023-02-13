95f42ed9a02b7b59631880905787eb5145510c79
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-02-13 16:13:34 +0500

    .

--- src/Employee.js
+++ src/Employee.js
@@ -17,11 +17,41 @@ function Employee() {
                 <input type={"text"} className="purple" value={'Hours / Days'} disabled></input>
             </section>
             <section className='tableBody'>
+                <div className='tr'>
+                    <form>
+                    <th>
+                        <input type='text' class='orange' placeholder='Enter Day'></input>
+                    </th>
+                    <td>
+                        <input type='time' class='green' id='start-work' ></input>
+                    </td>
+                    <td>
+                        <input type='time' class='blue' id='start-break' ></input>
+                    </td>
+                    <td>
+                        <input type='time' class='blue' id='end-breakk' ></input>
+                    </td>
+                    <td>
+                        <input type='time' class='blue' id='end-work' ></input>
+                    </td>
+                    <td>
+                        <input class='WorkedHours purple' value={"00:00"} disabled></input>
+                    </td>
+                    <td>
+                        <button class='btn' type='submit'>add</button>
+                    </td>
+                    </form>
+                </div>
                 <div className='tr'></div>
                 <div className='tr'></div>
                 <div className='tr'></div>
                 <div className='tr'></div>
                 <div className='tr'></div>
+                <div className='tr'></div>
+            </section>
+            <section className='tableFooter'>
+                <input  class='red' value="Total" disabled></input>
+                <input  id="totalWorkedHours" class='red' value="Work Hours" disabled></input>
             </section>
         </div>
     </div>
