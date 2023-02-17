19464ba051b0b87ee027bca9a7489306e110bf68
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-02-17 10:47:54 +0500

    Removed Vanilla Js.

--- src/Employee.js
+++ src/Employee.js
@@ -1,43 +1,7 @@
 import React from 'react'
 
 function Employee() {
-    // const tableBodyTrs = document
-    // .querySelectorAll(".tableBody")
-    // .querySelectorAll(".tr");
-    // console.log("tableBodyTrs");
 
-    // function createForm() {
-    //     let form = document.createElement("form");
-
-    //     form.innerHtml=`
-    //     <th>
-    //     <input type='text' class='orange' placeholder='Enter Day'></input>
-    // </th>
-    // <td>
-    //     <input type='time' class='green' id='start-work' ></input>
-    // </td>
-    // <td>
-    //     <input type='time' class='blue' id='start-break' ></input>
-    // </td>
-    // <td>
-    //     <input type='time' class='blue' id='end-breakk' ></input>
-    // </td>
-    // <td>
-    //     <input type='time' class='blue' id='end-work' ></input>
-    // </td>
-    // <td>
-    //     <input class='WorkedHours purple' value={"00:00"} disabled></input>
-    // </td>
-    // <td>
-    //     <button class='btn' type='submit'>add</button>
-    // </td>
-    //     `;
-
-    //     return form;
-    // }
-    // (async ()=> {
-    //     tableBodyTrs.forEach((tr) => tr.appendChild(createForm()));
-    // })();
   return (
     <div>
         <div className='container'>
@@ -95,4 +59,4 @@ function Employee() {
   )
 }
 
-export default Employee
+export default Employee;
